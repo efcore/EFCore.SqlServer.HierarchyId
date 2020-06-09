071cf4c2e0f4d47997d932809c3b5d4a3cc00dcf
cutig3r <cody.gentry@gmail.com>
2020-06-09 01:33:25 -0400

    Updated more commends

--- EFCore.SqlServer.HierarchyId.Test/Test/Operators/OperandExtensions.cs
+++ EFCore.SqlServer.HierarchyId.Test/Test/Operators/OperandExtensions.cs
@@ -2,6 +2,12 @@
 {
     public static class OperandExtensions
     {
+        /// <summary>
+        /// returns a new value tuple with the left-hand side and right-hand side values swapped
+        /// </summary>
+        /// <typeparam name="TOperand"></typeparam>
+        /// <param name="operands"></param>
+        /// <returns></returns>
         public static (TOperand lhs, TOperand rhs) Swap<TOperand>(this (TOperand lhs, TOperand rhs) operands)
             => (operands.rhs, operands.lhs);
 
