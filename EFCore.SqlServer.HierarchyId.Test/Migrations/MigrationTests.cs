8aa191e25ee1612645b6005f81e61d933f518060
cutig3r <Cody.Gentry@gmail.com>
2020-06-17 00:58:46 -0400

    Simplified migration tests

---
+++ EFCore.SqlServer.HierarchyId.Test/Migrations/MigrationTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Design.Internal;
+using Microsoft.EntityFrameworkCore.Migrations.Design;
+using Microsoft.EntityFrameworkCore.SqlServer.Migrations.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Xunit;
+
+namespace Microsoft.EntityFrameworkCore.SqlServer.Migrations
+{
+    public class MigrationTests
+    {
+        [Fact]
+        public void Migration_and_snapshot_generate_with_typed_array()
+        {
+            using var db = new MigrationContext1();
+            ValidateSnapshotAndMigrationCode(db);
+        }
+
+
+        [Fact]
+        public void Migration_and_snapshot_generate_with_anonymous_array()
+        {
+            using var db = new MigrationContext2();
+            ValidateSnapshotAndMigrationCode(db);
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "EF1001:Internal EF Core API usage.", Justification = "Uses internal efcore apis")]
+        private void ValidateSnapshotAndMigrationCode(MigrationContext context)
+        {
+            const string migrationName = "MyMigration";
+            const string @namespace = "MyApp.Data";
+
+            var expectedMigration = context.GetExpectedMigrationCode(migrationName, @namespace);
+            var expectedSnapshot = context.GetExpectedSnapshotCode(@namespace);
+
+            var reporter = new OperationReporter(
+                new OperationReportHandler(
+                    m => Console.WriteLine("  error: " + m),
+                    m => Console.WriteLine("   warn: " + m),
+                    m => Console.WriteLine("   info: " + m),
+                    m => Console.WriteLine("verbose: " + m)));
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            var servicesBuilder = new DesignTimeServicesBuilder(
+                assembly: assembly,
+                startupAssembly: assembly,
+                reporter: reporter,
+                args: Array.Empty<string>()
+            );
+            
+            var services = servicesBuilder.Build(context);
+            var scaffolder = services.GetRequiredService<IMigrationsScaffolder>();
+
+            var migration = scaffolder.ScaffoldMigration(
+                migrationName,
+                @namespace);
+
+            Assert.Equal(expectedMigration, migration.MigrationCode);
+            Assert.Equal(expectedSnapshot, migration.SnapshotCode);
+        }
+    }
+}
---
+++ EFCore.SqlServer.HierarchyId.Test/Migrations/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace Microsoft.EntityFrameworkCore.SqlServer.Migrations.Models
+{
+    internal sealed class Person
+    {
+        public HierarchyId Id { get; set; }
+        public string Name { get; set; }
+    }
+}
